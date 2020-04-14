c900f7baef653f687b2cade3c7735141ba8af3fd
ida4594 <56029881+ida4594@users.noreply.github.com>
2020-04-14 22:20:50 +0900

    Revert "CSVインポート"

--- db/migrate/20200414122345_create_sales.rb
+++
@@ -1,9 +0,0 @@
-class CreateSales < ActiveRecord::Migration[5.2]
-  def change
-    create_table :sales do |t|
-      t.integer      :code
-      t.integer      :price
-      t.timestamps null: true
-    end
-  end
-end
